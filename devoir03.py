22d627bf46c226f6ad346a5b9e3442b12146fb1d
ixe013 <guillaume@paralint.com>
2024-09-21 13:02:31 -0400

    Added interactive mode and icon

--- devoir03.py
+++ devoir03.py
@@ -1,3 +1,4 @@
+import fileinput
 import re
 import sys
 
@@ -18,7 +19,7 @@ def resolve(domain, record):
 def find_regex_value_in_records(regex, records):
     for record in records:
         if regex.search(record):
-            return str(record)
+            return f"✅ {str(record)}"
     else:
         return None
 
@@ -30,13 +31,18 @@ def is_proton_mail(mx):
 def has_spf1(txt):
     return find_regex_value_in_records(spf1, txt)
 
+def verify_domain(domain):
+    print(f"MX:  {is_proton_mail(resolve(domain, 'MX'))}")
+    print(f"TXT: {has_spf1(resolve(domain, 'TXT'))}")
 
 def main(argv):
-    for domain in argv[1:]:
-        mx = resolve(domain, "MX")
-        txt = resolve(domain, "TXT")
-        print(f"MX:  {is_proton_mail(mx)}")
-        print(f"TXT: {has_spf1(txt)}")
+    if len(argv) > 1:
+        for domain in argv[1:]:
+            verify_domain(domain)
+    else:
+        for domain in fileinput.input():
+            verify_domain(domain.strip())
+
 
 if __name__ == '__main__':
     main(sys.argv)
